ebab64615402354ac1f87c438cdfc0a31608c95d
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-09-10 01:56:07 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log('hello from type');
+var number = [23, 54, 64, 63, 76];
+console.log(number);
---
+++ js/app.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('hello from type');
+const number = [23, 54, 64, 63, 76];
+console.log(number);
