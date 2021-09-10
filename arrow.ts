1f9a287b7d192f16da8c96052b70a18444de1beb
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-09-10 20:32:21 +0600

    update

---
+++ arrow.ts
@@ -0,0 +1,12 @@
+function addNew(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+const add2 = (num1: number, num2: number): number => num1 + num2;
+
+const doMath = (x: number, y: number): void => {
+
+}
+const fullName = (fName:string , lName:string) : string =>{
+    return fName + lName;
+}
---
+++ js/arrow.js
@@ -0,0 +1,10 @@
+"use strict";
+function addNew(num1, num2) {
+    return num1 + num2;
+}
+const add2 = (num1, num2) => num1 + num2;
+const doMath = (x, y) => {
+};
+const fullName = (fName, lName) => {
+    return fName + lName;
+};
