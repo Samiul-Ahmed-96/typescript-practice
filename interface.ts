cf6f0dbdeab83af0fce641619486b8aa52bb9501
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-03-09 22:09:42 +0600

    update

--- interface.ts
+++ interface.ts
@@ -1,43 +1,54 @@
 interface Player {
-    name: string,
-    club: string,
-    salary: number,
-    wife?: string,
-    previousClubs?: string[]
+  name: string;
+  club: string;
+  salary: number;
+  wife?: string;
+  previousClubs?: string[];
 }
 
 const messy: Player = {
-    name: 'L Messy',
-    club: 'Abahoni',
-    salary: 12000,
-    wife: 'Nosimon ara begum'
-}
+  name: "L Messy",
+  club: "Abahoni",
+  salary: 12000,
+  wife: "Nosimon ara begum",
+};
 const hannan: Player = {
-    name: 'A. Hannan',
-    club: 'Kolabagan',
-    salary: 2000
-}
-
+  name: "A. Hannan",
+  club: "Kolabagan",
+  salary: 2000,
+};
 
 interface Employee {
-    name: string,
-    designation: string,
-    salary: number
-    age: number
+  name: string;
+  designation: string;
+  salary: number;
+  age: number;
 }
 
 interface Developer extends Employee {
-    language: string,
-    codeEditor: string,
-    typingSpeed: number
+  language: string;
+  codeEditor: string;
+  typingSpeed: number;
 }
 
 const zukerberg: Developer = {
-    name: 'Zukerberg',
-    salary: 1100,
-    age: 31,
-    designation: 'Facebooker',
-    language: 'js',
-    codeEditor: 'VS Code',
-    typingSpeed: 65
+  name: "Zukerberg",
+  salary: 1100,
+  age: 31,
+  designation: "Facebooker",
+  language: "js",
+  codeEditor: "VS Code",
+  typingSpeed: 65,
+};
+
+interface Iperson {
+  name: string;
+  phone: number;
+  //optional object
+  hobby?: string;
 }
+
+const personOne: Iperson = {
+  name: "Anik",
+  phone: 999220,
+};
---
+++ js/class.js
@@ -0,0 +1,11 @@
+"use strict";
+class car {
+    constructor(name, model, price) {
+        this.name;
+        name;
+        this.model;
+        model;
+        this.price;
+        price;
+    }
+}
